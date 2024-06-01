b1711f2d6296cfa45f31443550f6f20263cd0bfd
Scar <scarcheekttgm@gmail.com>
2024-06-01 13:55:10 +0200

    Chair added

---
+++ Assets/Scripts/Card/CardList/Chair.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chair : CardBehaviour
+{
+    [SerializeField] private float valIncrease;
+
+    internal override bool OnAddToPlayZone()
+    {
+        if (!base.OnAddToPlayZone()) return false;
+        EventManager.EmitAllOfMediumEffect(Medium.chair, ChairValIncrease);
+        return true;
+    }
+
+    internal override bool MediumEffect(CardBehaviour card)
+    {
+        return ChairValIncrease(card);
+
+    }
+    internal override bool RevertMediumEffect(CardBehaviour card)
+    {
+        return RevertChairValIncrease(card);
+    }
+
+    internal bool ChairValIncrease(CardBehaviour cardToBuff)
+    {
+        if (cardToBuff != this && !buffedTypeCards.Contains(cardToBuff) && cardToBuff.cardProps.medium.Contains(Medium.chair))
+        {
+            cardToBuff.cardValue += valIncrease;
+            return ApplyEffect(buffedTypeCards, cardToBuff);
+        }
+        return false;
+    }
+
+    internal bool RevertChairValIncrease(CardBehaviour card)
+    {
+        if (card != this && buffedTypeCards.Contains(card))
+        {
+            card.cardCost -= valIncrease;
+            return ApplyRevert(card);
+        }
+        return false;
+    }
+}
