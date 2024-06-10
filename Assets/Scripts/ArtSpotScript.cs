65a9dd9b5b1ab271fdae44c5c0eda14a5a5757a8
Scar <scarcheekttgm@gmail.com>
2024-06-10 12:10:44 +0200

    Spawning in the prefabs for implemented cards

--- Assets/Scripts/ArtSpotScript.cs
+++ Assets/Scripts/ArtSpotScript.cs
@@ -22,6 +22,10 @@ public class ArtSpotScript : MonoBehaviour
         {
             Debug.Log("presenting");
             artPiece = Instantiate(card.card.cardProps.presentPrefab, transform, false);
+            if (card.card.cardProps.medium.Contains(Medium.painting) || card.card.cardProps.medium.Contains(Medium.music))
+            {
+                artPiece.transform.localPosition = new Vector3(0.0f, 1, 0.0f); 
+            }
         }
     }
 
